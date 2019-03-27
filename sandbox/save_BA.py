db9c0be226b7dbf62d1ca6c05a01d96e237a62c4
Benjamin Maier <benjaminfrankmaier@gmail.com>
2019-03-27 13:15:40 +0100

    changed drawing

--- sandbox/save_BA.py
+++ sandbox/save_BA.py
@@ -4,7 +4,7 @@ import json
 
 G = nx.barabasi_albert_graph(1000,2)
 
-props, config = visualize(G)
+props, config = visualize(G,config={'Node size by strength':True,'Collision':True,'Node size': 25})
 with open('BA_network_properties.json', 'w') as outfile:
     json.dump(props, outfile)
 
