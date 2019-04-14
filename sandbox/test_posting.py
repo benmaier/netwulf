2a358640ac7c389580ff989ff0935aae005522d5
Benjamin Maier <benjaminfrankmaier@gmail.com>
2019-04-13 21:38:27 -0600

    advanced some test

--- sandbox/test_posting.py
+++ sandbox/test_posting.py
@@ -1,4 +1,6 @@
 from netwulf.tools import bind_positions_to_network
+import networkx as nx
+
 if __name__ == "__main__":
     import pprint 
     pp = pprint.PrettyPrinter(indent=4)
@@ -6,10 +8,12 @@ if __name__ == "__main__":
     G = nx.Graph()
     G.add_nodes_from(range(10))
     G.add_nodes_from("abcde")
+    G.add_edges_from([("a","b")])
 
     from netwulf import visualize
     props, config = visualize(G)
 
     pp.pprint(props)
+    pp.pprint(config)
     bind_positions_to_network(G, props)
     visualize(G, config=config)
