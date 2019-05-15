de136d64fd2c6db4561d601d0b43b491af460571
Benjamin Maier <benjaminfrankmaier@gmail.com>
2019-05-15 10:13:35 +0200

    v++

--- netwulf/metadata.py
+++ netwulf/metadata.py
@@ -3,7 +3,7 @@
 Contains a bunch of information about this package.
 """
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 __author__ = "Ulf Aslak, Benjamin F. Maier"
 __copyright__ = "Copyright 2018, Ulf Aslak, Benjamin F. Maier"
